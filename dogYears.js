71285078bd6953f963763c8d9ee0e235a655ce5e
Chittajallu Chinmai Ganesh <46193776+chcg823@users.noreply.github.com>
2020-07-26 18:43:43 +0530

    Create dogYears.js

---
+++ dogYears.js
@@ -0,0 +1,14 @@
+//My age
+let myAge = 22;
+//Early years of Dog = 2
+let earlyYears = 2;
+earlyYears *= 10.5;
+//Later years of Dog
+let laterYears = myAge - 2;
+//multiply later year by 4 since one year = 4 dog years
+laterYears *= 4;
+//Calculating Dog age.
+let myAgeInDogYears = laterYears + earlyYears;
+//my Name
+let myName = 'Chinmai'.toLowerCase()
+console.log(`My name is ${myName}. I am {myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`);
